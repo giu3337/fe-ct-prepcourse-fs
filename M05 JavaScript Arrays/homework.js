13781d122cd25bb63515fe13c4a261c35fcc3619
giu3337 <72093755+giu3337@users.noreply.github.com>
2023-01-23 16:01:01 -0500

    Actualizar

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -3,34 +3,45 @@
 function devolverPrimerElemento(array) {
    // Retornar el primer elemento del arreglo recibido por parámetro.
    // Tu código:
+   return array.shift();
+
 }
 
 function devolverUltimoElemento(array) {
    // Retornar el último elemento del arreglo recibido por parámetro.
    // Tu código:
+   return array.pop(); 
 }
 
 function obtenerLargoDelArray(array) {
    // Retornar la longitud del arreglo recibido por parámetro.
    // Tu código:
+   return array.length; 
 }
 
 function incrementarPorUno(array) {
    // El arreglo recibido por parámetro contiene números.
    // Retornar un arreglo con los elementos incrementados en +1.
    // Tu código:
+   return array.map((num) => {
+      return num + 1; 
+   })
 }
 
 function agregarItemAlFinalDelArray(array, elemento) {
    // Agrega el "elemento" al final del arreglo recibido.
    // Retorna el arreglo.
    // Tu código:
+   array.push(elemento)
+   return array; 
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
    // Agrega el "elemento" al comienzo del arreglo recibido.
    // Retorna el arreglo.
    // Tu código:
+   array.unshift(elemento)
+   return array; 
 }
 
 function dePalabrasAFrase(palabras) {
@@ -39,30 +50,50 @@ function dePalabrasAFrase(palabras) {
    // con un espacio entre cada palabra.
    // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'.
    // Tu código:
+
+   let palabrasUnidas = palabras.join(' ')
+   return  palabrasUnidas; 
 }
 
 function arrayContiene(array, elemento) {
    // Verifica si el elemento existe dentro del arreglo recibido.
    // Retornar True si está, o False si no está.
    // Tu código:
+    return array.includes(elemento)
+    
+     
 }
 
 function agregarNumeros(arrayOfNums) {
    // El parámetro "arrayOfNums" debe ser un arreglo de números.
    // Suma todos los elementos y retorna el resultado.
    // Tu código:
+   let suma = 0 
+   for (let i = 0; i < arrayOfNums.length; i++) {
+      const element = array[i] ;
+      
+   }
 }
 
 function promedioResultadosTest(resultadosTest) {
    // El parámetro "resultadosTest" es un arreglo de números.
    // Itera (en un bucle) los elementos del arreglo y devuelve el promedio de las notas.
    // Tu código:
+   let suma = 0; 
+   for (let i = 0; i < resultadosTest.length; i++) {
+      suma = suma + resultadosTest[i] // suma += resultadosTest[i]
+      
+   }
+         return suma / resultadosTest.length
 }
 
 function numeroMasGrande(arrayOfNums) {
    // El parámetro "arrayOfNums" es un arreglo de números.
    // Retornar el número más grande.
    // Tu código:
+
+  
+   
 }
 
 function multiplicarArgumentos() {
--- PracticaJs/practica.js
+++ PracticaJs/practica.js
@@ -4,6 +4,11 @@
 // Bucles 
 // Acciones repetitivas
 
+let arreglo = ['hola' , 'holatu' , 'holahola'];
+ let palabrasUnidas = arreglo.join(' ')
+console.log(palabrasUnidas);
+let metodoInclude = arreglo.include('hola')
+
 
 // For
 
@@ -29,3 +34,134 @@ function sacar3digitos (num) {
 }
 
 console.log(sacar3digitos(22));
+
+
+// Arrays 
+
+let listaDeCompras = [];
+listaDeCompras[3] = 'Tomates'; 
+listaDeCompras[1] = 'Lechuga'
+console.log(listaDeCompras.length);
+
+
+// console.log(listaDeCompras);
+
+// let elementoDelArray = listaDeCompras[1];
+
+// console.log(elementoDelArray); 
+
+
+// Metodos Arrays 
+
+//Split 
+
+let palabra = 'Henri'; 
+let palabraSeparada = palabra.split('') // separar la palabra por cada espacio
+console.log(palabraSeparada);
+palabraSeparada.pop();
+palabraSeparada.push('y')
+console.log(palabraSeparada);
+let palabraArreglada = palabraSeparada.join('')
+console.log(palabraArreglada);
+
+let numeros = [1,2,3,4];
+// numeros.forEach((num) => console.log(num)); 
+// numeros.forEach(num => {if(num === 3) console.log(num)})
+
+let masUno = numeros.map((num) => { // se puede hacer cambios 
+    return num + 1; 
+})
+
+console.log(masUno);
+
+
+// Bucles Arrelos 
+
+
+// For 
+
+// let array = [1,2,3,4,5]; 
+// for (let i = 0; i < array.length; i++) {
+//     console.log(array[i]); // accion sobre cada elemento
+    
+// }
+
+ function encontrarLetraP (string) {
+    let letras = string.split('')
+    
+    for (let i = 0; i < letras.length; i++) {
+        if (letras[i] === 'p')
+        return 'Si contiene una P'; 
+         {
+            
+        }
+        
+    }
+ }
+
+console.log(encontrarLetraP('javascript'));
+console.log(encontrarLetraP('Java'));
+
+
+// While 
+
+let arr = []; 
+while (arr.length < 5) { // condiciones 
+    arr.push('Boom')
+}
+
+console.log(arr);
+/* 
+Métodos de inserción
+
+1
+
+1
+El método push() añade uno o más elementos al final de un arreglo, y devuelve la nueva longitud del array.
+
+2
+
+2
+El método pop() elimina y devuelve el último elemento de un arreglo.
+
+3
+
+3
+El método unshift() agrega uno o más elementos al inicio de un arreglo, y devuelve la nueva longitud del array.
+
+4
+
+4
+El método shift() elimina y devuelve el primer elemento de un arreglo.
+
+Métodos varios
+
+1
+
+1
+El método includes() determina si un arreglo incluye o contiene un elemento específico. Devuelve true o false en cada caso.
+
+2
+
+2
+El método every() determina si todos los elementos en un arreglo satisfacen una misma condición.
+
+3
+
+3
+El método split() convierte un string en un arreglo, donde cada elemento contendrá un sub-string, dependiendo del parámetro divisor que indiquemos.
+
+4
+
+4
+El método join() convierte un arreglo en un string, uniendo todos los elementos de este en una misma cadena.
+*/
+
+
+ 
+
+
+
+
+
+//Join
